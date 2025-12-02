88334a85661ae22862212e472820a82713fac953
n0pen <noscandev@protonmail.com>
2025-12-02 19:29:04 -0300

    Day 1 part 2

--- day1/d1p1.go
+++ day1/d1p1.go
@@ -2,24 +2,34 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
+const dialLength = 100
+
 func main() {
-	position := 100000000 + 50
+	position := 50
 	file, _ := os.ReadFile("./day1/input")
 	counter := 0
 	filestr := strings.Replace(string(file), "\r\n", "\n", -1)
 	for _, str := range strings.Split(filestr, "\n") {
+
 		str = strings.Replace(str, "L", "-", -1)
 		str = strings.Replace(str, "R", "", -1)
-		val, _ := strconv.ParseInt(str, 10, 64)
-		position += int(val)
-		if position%100 == 0 {
+		rotation, _ := strconv.ParseInt(str, 10, 64)
+		times := int(math.Abs(float64(int(rotation) / dialLength)))
+		counter += times
+		newval := int(rotation) % dialLength
+		newPosition := position + newval
+
+		if newPosition > dialLength || (position > 0 && newPosition < 0) || newPosition%dialLength == 0 {
 			counter += 1
 		}
+		position = int(math.Abs(float64((newPosition + dialLength) % dialLength)))
+
 	}
 	fmt.Println(counter)
 
